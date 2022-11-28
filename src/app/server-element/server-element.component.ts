1d582088e02120b31b505e3fa4023ce31f9f28b3
Sdrapinski <szymondrapinski@gmail.com>
2022-11-28 23:56:15 +0100

    passing data

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
-  encapsulation: ViewEncapsulation.ShadowDom, // zmienia działanie css (globalny localny itp)
+  // encapsulation: ViewEncapsulation.Emulated, // zmienia działanie css (globalny localny itp)
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: { type: string; name: string; content: string };
